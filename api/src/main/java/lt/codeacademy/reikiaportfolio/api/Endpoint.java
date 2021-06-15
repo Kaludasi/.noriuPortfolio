996358e42f9ddb7d4e7798efcdd0b248a4534ef5
Kaludasi <simo.klaudijus@gmail.com>
2021-06-15 20:24:05 +0300

    add endpoint interface

---
+++ api/src/main/java/lt/codeacademy/reikiaportfolio/api/Endpoint.java
@@ -0,0 +1,13 @@
+package lt.codeacademy.reikiaportfolio.api;
+
+public interface Endpoint {
+    String PRODUCT_ID = "id";
+    String ORDER_ID = "id";
+    String GET_PRODUCT_BY_ID_PATH = "/{id}";
+    String GET_ORDER_BY_ID_PATH = "/{id}";
+    String PRODUCTS = "/products";
+    String ORDERS = "/orders";
+    String SIGNUP = "/signup";
+    String LOGOUT = "/logout";
+    String PERSON = "/person";
+}
