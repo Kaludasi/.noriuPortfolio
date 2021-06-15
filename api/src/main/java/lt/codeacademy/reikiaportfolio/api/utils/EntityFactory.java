4a43d43b1abb4a308534482d11fb650016dff904
Kaludasi <simo.klaudijus@gmail.com>
2021-06-15 20:22:06 +0300

    add entity map utils

---
+++ api/src/main/java/lt/codeacademy/reikiaportfolio/api/utils/EntityFactory.java
@@ -0,0 +1,41 @@
+package lt.codeacademy.reikiaportfolio.api.utils;
+
+import lt.codeacademy.reikiaportfolio.persistence.entity.PersonOrder;
+import lt.codeacademy.reikiaportfolio.persistence.entity.Person;
+import lt.codeacademy.reikiaportfolio.persistence.entity.Product;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+public class EntityFactory {
+
+    public static Person getPerson(String name, String surname, String email, String password, String phone, List<PersonOrder> orders) {
+        return Person.builder()
+                .name(name)
+                .surname(surname)
+                .email(email)
+                .password(password)
+                .phone(phone)
+                .orders(orders)
+                .build();
+    }
+
+    public static Product getProduct(UUID id, String name, String description, BigDecimal price) {
+        return Product.builder()
+                .id(id)
+                .name(name)
+                .description(description)
+                .price(price)
+                .build();
+    }
+
+    public static PersonOrder getOrder(Product product, Person customer, String productDescription, String status) {
+        return PersonOrder.builder()
+                .product(product)
+                .customer(customer)
+                .productDescription(productDescription)
+                .status(status)
+                .build();
+    }
+}
---
+++ api/src/main/java/lt/codeacademy/reikiaportfolio/api/utils/EntityMapper.java
@@ -0,0 +1,75 @@
+package lt.codeacademy.reikiaportfolio.api.utils;
+
+import lt.codeacademy.reikiaportfolio.api.dto.*;
+import lt.codeacademy.reikiaportfolio.persistence.entity.Person;
+import lt.codeacademy.reikiaportfolio.persistence.entity.Product;
+import lt.codeacademy.reikiaportfolio.persistence.entity.PersonOrder;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public interface EntityMapper {
+
+    static Person map(UserRegistrationDTO userRegistration) {
+        return EntityFactory.getPerson(
+                userRegistration.getName(),
+                userRegistration.getSurname(),
+                userRegistration.getEmail(),
+                userRegistration.getPassword(),
+                userRegistration.getPhone(),
+                List.of());
+    }
+
+    static Product map(ProductDTO productDTO) {
+        return EntityFactory.getProduct(
+                productDTO.getId(),
+                productDTO.getName(),
+                productDTO.getDescription(),
+                productDTO.getPrice());
+    }
+
+    static PersonOrder map(CreateOrderDTO createOrderDTO, Person person, Product product) {
+        return EntityFactory.getOrder(
+                product,
+                person,
+                createOrderDTO.getProductDescription(),
+                createOrderDTO.getStatus());
+    }
+
+    static UserOrderDTO map(PersonOrder personOrder) {
+        return UserOrderDTO.builder()
+                .id(personOrder.getId())
+                .customer(map(personOrder.getCustomer()))
+                .product(map(personOrder.getProduct()))
+                .productDescription(personOrder.getProductDescription())
+                .status(personOrder.getStatus())
+                .build();
+    }
+
+    static ProductDTO map(Product product) {
+        return ProductDTO.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
+
+    static PersonInfoDTO map(Person person) {
+        return PersonInfoDTO.builder()
+                .name(person.getName())
+                .surname(person.getSurname())
+                .orders(mapOrderList(person.getOrders()))
+                .email(person.getEmail())
+                .phone(person.getPhone())
+                .build();
+    }
+
+    static List<UserOrderDTO> mapOrderList(List<PersonOrder> orders) {
+        return orders.stream().map(EntityMapper::map).collect(Collectors.toList());
+    }
+
+    static List<ProductDTO> mapProductList(List<Product> products) {
+        return products.stream().map(EntityMapper::map).collect(Collectors.toList());
+    }
+}
