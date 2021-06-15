e04e2c17471e17f4d8ce7dfb67f0262bd65384b1
Kaludasi <simo.klaudijus@gmail.com>
2021-06-15 20:23:42 +0300

    add main

---
+++ api/src/main/java/lt/codeacademy/reikiaportfolio/ReikiaPortfolioApplication.java
@@ -0,0 +1,13 @@
+package lt.codeacademy.reikiaportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReikiaPortfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReikiaPortfolioApplication.class, args);
+    }
+
+}
